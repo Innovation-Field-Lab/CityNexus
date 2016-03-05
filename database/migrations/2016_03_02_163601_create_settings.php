8d85620b752986e8d92b1c595e5a7f1ec5a636b8
Sean Alaback <sean.alaback@gmail.com>
2016-03-04 22:27:09 -0500

    fixed typo in create settings class

--- database/migrations/2016_03_02_163601_create_settings.php
+++ database/migrations/2016_03_02_163601_create_settings.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Settings extends Migration
+class CreateSettings extends Migration
 {
     /**
      * Run the migrations.

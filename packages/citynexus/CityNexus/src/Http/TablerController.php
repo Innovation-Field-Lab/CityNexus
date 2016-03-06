51ee361a926e91d97e30960604a2246c07171f38
Sean Alaback <sean.alaback@gmail.com>
2016-03-06 13:32:55 -0500

    test excel upload

--- packages/citynexus/CityNexus/src/Http/TablerController.php
+++ packages/citynexus/CityNexus/src/Http/TablerController.php
@@ -33,8 +33,7 @@ class TablerController extends Controller
 
         $table = Excel::load($request->file('file'), function($reader){$reader->toArray();});
 
-        dd($table);
-
+        dd(json_encode(end($table));
 
         $table = Table::create(['raw_upload' => json_encode(end($table))]);
 

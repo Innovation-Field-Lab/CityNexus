41ba17b54e98765932cc64207c63fd53c53d5151
Sean Alaback <sean.alaback@gmail.com>
2016-03-28 15:21:40 -0400

    Updated develop

--- packages/citynexus/CityNexus/src/views/devlog.blade.php
+++ packages/citynexus/CityNexus/src/views/devlog.blade.php
@@ -4,14 +4,20 @@
 - Add Scores to property record views<br>
 - Remove aliases from property lists<br>
 - Debug Score Builder with Date/Time objects<br>
+- Edit user accounts<br>
 <br>
 
 <b>Stretch Goals</b><br>
 - Pin Maps<br>
 - Edit and reassociate records<br>
 - Change the primary property record<br>
+- Hard delete of datasets<br>
 
 <br>
 <b>Wait List</b><br>
-- City Dashboard
-- Queued Jobs Complete Email/Notification
+- City Dashboard<br>
+- Queued Jobs Complete Email/Notification<br>
+- API Generation<br>
+- Public Dashboards<br>
+- City Dashboards<br>
+- Click-2-Action Buttons<br>

ed6c402862a0f532bec5dca9a41b4a1557c66fa6
Sean Alaback <sean.alaback@gmail.com>
2016-03-28 10:29:55 -0400

    updated devlog

--- packages/citynexus/CityNexus/src/views/devlog.blade.php
+++ packages/citynexus/CityNexus/src/views/devlog.blade.php
@@ -1,6 +1,10 @@
 <b>Next Sprint</b><br>
 - Granular permissions<br>
 - Data downloader<br>
+- Edit and reassociate records<br>
+- Add Scores to property record views<br>
+- Remove aliases from property lists<br>
+- Change the primary property record<br>
 <br>
 
 <b>Stretch Goals</b><br>

3982add24a7522c5910f31763a10d4ecc559633c
Sean Alaback <sean.alaback@gmail.com>
2016-03-29 18:42:02 -0400

    Add Admin Routes

---
+++ packages/citynexus/CityNexus/src/views/admin/edit-table.blade.php
@@ -0,0 +1,50 @@
+
+@extends(config('citynexus.template'))
+
+@section(config('citynexus.section'))
+
+    <div class="panel panel-default">
+        <div class="panel-heading" style="height: 60px">
+            <span class="panel-title">{{$table_name}}</span>
+
+            <form action="/{{config('citynexus.root_directory')}}/admin/edit-table" class="form-inline pull-right">
+                {{csrf_field()}}
+                <select name="table_name" id="table_name" class="form-control">
+                    <option value="">Select One</option>
+                    @foreach($tables as $i)
+                        <option value="{{$i->table_name}}">{{$i->table_name}}</option>
+                    @endforeach
+                </select>
+                <input type="submit" class="btn btn-primary" value="Submit">
+        </div>
+        <div class="panel-body" style="overflow: scroll;">
+            @if(count($table) > 0)
+            <table class="table table-bordered table-striped">
+                <tr>
+                    @foreach($table[0] as $k => $i)
+                        <th>
+                            {{$k}}
+                        </th>
+                    @endforeach
+                </tr>
+                <tbody>
+                @foreach($table as $row)
+                    <tr>
+                        @foreach($row as $item)
+                            <td>
+                                {{$item}}
+                            </td>
+                        @endforeach
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+                @else
+            <div class="alert alert-info">
+                Table currently holds no records.
+            </div>
+                @endif
+        </div>
+    </div>
+
+    @stop

ee08fdccd3b3110eda734204b412dc9c1d8a9b52
Sean Alaback <sean.alaback@gmail.com>
2016-03-29 13:31:58 -0400

    Added confirmation that a sync field has been set

--- packages/citynexus/CityNexus/src/views/releasenote.blade.php
+++ packages/citynexus/CityNexus/src/views/releasenote.blade.php
@@ -1,5 +1,8 @@
 <b>v0.1.1 - Mon. 29 March 2016 12:30</b></b><br>
-- Fixed link to data sets from property profile view
+- Fixed link to data sets from property profile view<br>
+- Fixed broken remove element method on score builder<br>
+- Ignore dataset record uploads with no associated sync fields<br>
+- Added confirmation that a sync field has been set to schema builder<br>
 
 <b>v0.1.0 - Mon. 28 March 2016 12:30</b></b><br>
 - Authentication Layer<br>
--- packages/citynexus/CityNexus/src/views/tabler/create-scheme.blade.php
+++ packages/citynexus/CityNexus/src/views/tabler/create-scheme.blade.php
@@ -55,10 +55,27 @@
                                 @endforeach
                         </tbody>
                     </table>
-                    <input type="submit" class="btn btn-primary" value="Save and Complete Upload">
+                    <div class="form-group alert alert-info" id="syncCheck">
+                        <label for="sync_field" class="control-label col-sm-4">Please verify your sync fields</label>
+                        <div class='btn btn-primary' onclick="enableSubmit()">I did!</div>
+                    </div>
+                    <input type="submit" class="btn btn-primary hidden" id="submit" value="Save and Complete Upload">
                 </form>
             </div>
         </div>
     </div>
+
+@stop
+
+@push('js_footer')
+
+<script>
+    function enableSubmit()
+    {
+        $('#syncCheck').addClass('hidden');
+        $('#submit').removeClass('hidden');
+
+    }
+</script>
 
 @stop

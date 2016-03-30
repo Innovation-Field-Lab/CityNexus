b5b523b657d59c91ba06e0975ce3c5ff31fb3d01
Sean Alaback <sean.alaback@gmail.com>
2016-03-30 10:07:58 -0400

    Removed extra error

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -49,15 +49,6 @@
 
                             <form method="POST" action="/password/email">
                                 {!! csrf_field() !!}
-
-                                @if (count($errors) > 0)
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                @endif
-
                                 <div>
                                     <label for="email">Email</label>
                                     <input type="email" class="form-control" name="email" value="{{ old('email') }}">

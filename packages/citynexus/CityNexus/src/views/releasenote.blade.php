91cf26e23b7c3dd9c1eb1a2d317981230f53a2a9
Sean Alaback <sean.alaback@gmail.com>
2016-03-30 15:41:27 -0400

    Updated Dev Log

--- packages/citynexus/CityNexus/src/views/releasenote.blade.php
+++ packages/citynexus/CityNexus/src/views/releasenote.blade.php
@@ -1,11 +1,14 @@
+<b>v0.1.2 - Wend. 20 March 2016 15:45</b><br>
 - Created Admin view of all tables<br>
 - Fixed bug produced by stopping data set creation mid-process<br>
 - Removed alias properties from all properties list<br>
 - Create data download method<br>
 - Cleaned up the password reset method and added error reporting<br>
+- Fixed bug in schema sync dropdown<br>
+- Added new error reporting log with try catch blocks<br>
 <br>
 
-<b>v0.1.1 - Mon. 29 March 2016 12:30</b><br>
+<b>v0.1.1 - Tue. 29 March 2016 12:30</b><br>
 - Fixed link to data sets from property profile view<br>
 - Fixed broken remove element method on score builder<br>
 - Ignore dataset record uploads with no associated sync fields<br>

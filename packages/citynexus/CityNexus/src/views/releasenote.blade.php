f45b8557306ed3b830a6ddf111ca5a9f9cc46a0d
Sean Alaback <sean.alaback@gmail.com>
2016-03-30 10:15:05 -0400

    Edit dev log

--- packages/citynexus/CityNexus/src/views/releasenote.blade.php
+++ packages/citynexus/CityNexus/src/views/releasenote.blade.php
@@ -5,13 +5,13 @@
 - Cleaned up the password reset method and added error reporting<br>
 <br>
 
-<b>v0.1.1 - Mon. 29 March 2016 12:30</b></br><br>
+<b>v0.1.1 - Mon. 29 March 2016 12:30</b><br>
 - Fixed link to data sets from property profile view<br>
 - Fixed broken remove element method on score builder<br>
 - Ignore dataset record uploads with no associated sync fields<br>
 - Added confirmation that a sync field has been set to schema builder<br>
-
-<b>v0.1.0 - Mon. 28 March 2016 12:30</b></br><br>
+<br>
+<b>v0.1.0 - Mon. 28 March 2016 12:30</b><br>
 - Authentication Layer<br>
 - Data Uploader<br>
 - Property Data Cleaner<br>

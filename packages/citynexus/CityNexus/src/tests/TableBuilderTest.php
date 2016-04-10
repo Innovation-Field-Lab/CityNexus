0328b4e84929bc354be69516b3f3554104610976
Sean Alaback <sean.alaback@gmail.com>
2016-04-10 17:46:23 -0400

    Created test for unit in the house number

--- packages/citynexus/CityNexus/src/tests/TableBuilderTest.php
+++ packages/citynexus/CityNexus/src/tests/TableBuilderTest.php
@@ -8,6 +8,7 @@ use CityNexus\CityNexus\TableBuilder;
 class TablerControllerTest extends TestCase
 {
     use WithoutMiddleware;
+    use DatabaseTransactions;
 
     /**
      *
@@ -17,18 +18,48 @@ class TablerControllerTest extends TestCase
     public function testFindSyncID()
     {
         $tableBulider = new TableBuilder();
+        
         $syncValues = [
             'house_number' => 'house_number',
             'street_name' => 'street_name',
             'street_type' => 'street_type'];
+
         $i = ['house_number' => '123',
             'street_name' => 'testing',
             'street_type' => 'street'];
+
         $property = \CityNexus\CityNexus\Property::create([
+            'full_address' => '123 testing street',
             'house_number' => '123',
             'street_name' => 'testing',
             'street_type' => 'street'
         ]);
+
+        $result = $tableBulider->findSyncId('citynexus_properties', $i, $syncValues);
+
+        $this->assertSame($property->id, $result);
+    }
+
+
+    public function testUnitsInHouseNumbers()
+    {
+        $syncValues = [
+            'house_number' => 'house_number',
+            'street_name' => 'street_name',
+            'street_type' => 'street_type'];
+        $i = ['house_number' => '123 #5',
+            'street_name' => 'testing',
+            'street_type' => 'street'];
+
+        $property = \CityNexus\CityNexus\Property::create([
+            'full_address' => '123 testing street #5',
+            'house_number' => '123',
+            'street_name' => 'testing',
+            'street_type' => 'street',
+            'unit' => '#5'
+        ]);
+
+        $tableBulider = new TableBuilder();
 
         $result = $tableBulider->findSyncId('citynexus_properties', $i, $syncValues);
 

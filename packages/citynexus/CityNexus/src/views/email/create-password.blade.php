003acdb8fb3fe2c837d641512862c001944cc96c
Sean Alaback <sean.alaback@gmail.com>
2016-06-01 16:30:32 +0200

    Clean up auth page

--- packages/citynexus/CityNexus/src/views/email/create-password.blade.php
+++ packages/citynexus/CityNexus/src/views/email/create-password.blade.php
@@ -2,80 +2,43 @@
 
 @section('main')
 
-    <style>
-        img.bg {
-            /* Set rules to fill background */
-            min-height: 100%;
-            min-width: 1024px;
+<h3>Create Your Password</h3>
+<div class="alert alert-info">
+    To activate your account, please create a password.
+</div>
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 
-            /* Set up proportionate scaling */
-            width: 100%;
-            height: auto;
+<form class="form-horizontal" role="form" method="POST" action="{{ url('/activate-account/')}}">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <input type="hidden" name="token" value="{{$token}}" >
 
-            /* Set up positioning */
-            position: fixed;
-            top: 0;
-            left: 0;
-            z-index:-1;
-        }
-
-        @media screen and (max-width: 1024px) { /* Specific to this particular image */
-            img.bg {
-                left: 50%;
-                margin-left: -512px;   /* 50% */
-            }
-        }
-    </style>
-    <img src="/vendor/citynexus/img/background.png" class="bg hidden-xs">
-
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default" id="login">
-                    <div class="panel-heading">Create Your Password</div>
-                    <div class="panel-body">
-
-                        <div class="alert alert-info">
-                            To activate your account, please create a password.
-                        </div>
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/activate-account/')}}">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <input type="hidden" name="token" value="{{$token}}" >
-
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Password</label>
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" name="password">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Confirm Password</label>
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" name="confirm-password">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">Create Account</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+    <div class="form-group">
+        <label class="col-md-4 control-label">Password</label>
+        <div class="col-md-6">
+            <input type="password" class="form-control" name="password">
         </div>
     </div>
+
+    <div class="form-group">
+        <label class="col-md-4 control-label">Confirm Password</label>
+        <div class="col-md-6">
+            <input type="password" class="form-control" name="confirm-password">
+        </div>
+    </div>
+
+    <div class="form-group">
+        <div class="col-md-6 col-md-offset-4">
+            <button type="submit" class="btn btn-primary">Create Account</button>
+        </div>
+    </div>
+</form>
 @endsection

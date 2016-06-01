2aae185ee5ed1f9240255444f3fdfcbe07aac365
Sean Alaback <sean.alaback@gmail.com>
2016-06-01 16:19:46 +0200

    Add authviews

---
+++ public/resources/views/auth/password.blade.php
@@ -0,0 +1,23 @@
+<!-- resources/views/auth/password.blade.php -->
+
+@extends('citynexus::master.login')
+
+@section('main')
+
+<form method="POST" action="/password/email">
+    {!! csrf_field() !!}
+    <div>
+        <label for="email">Email</label>
+        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+    </div>
+    <br>
+    <div>
+        <button type="submit" class="btn btn-primary">
+            Send Password Reset Link
+        </button>
+    </div>
+</form>
+
+@endsection
+
+
---
+++ public/resources/views/auth/reset.blade.php
@@ -0,0 +1,33 @@
+<!-- resources/views/auth/reset.blade.php -->
+
+@extends('citynexus::master.login')
+
+@section('main')
+<form method="POST" action="/password/reset">
+    {!! csrf_field() !!}
+    <input type="hidden" class="form-control" name="token" value="{{ $token }}">
+    <div>
+        <label for="email">Email</label>
+        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input type="password" class="form-control" name="password">
+    </div>
+
+    <div>
+        <label for="password_confirmation">Confirm Password</label>
+        <input type="password" class="form-control" name="password_confirmation">
+    </div>
+    <br>
+
+    <div>
+        <button type="submit" class="btn btn-primary">
+            Reset Password
+        </button>
+    </div>
+</form>
+@endsection
+
+

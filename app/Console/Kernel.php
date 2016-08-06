55cbf3324ba2b8f2ff04ef895248660a5d39bbfb
Sean Alaback <sean.alaback@gmail.com>
2016-08-06 17:11:47 +0100

    Make dropbox upload daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 
-        $schedule->command('citynexus:upload daily')->everyMinute();
+        $schedule->command('citynexus:dropbox daily')->everyMinute();
     }
 }

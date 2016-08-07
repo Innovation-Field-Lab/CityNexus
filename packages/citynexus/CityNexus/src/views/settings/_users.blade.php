c1f41c8bea40fb63b971433dfdbae24263968a76
Sean Alaback <sean.alaback@gmail.com>
2016-08-07 10:11:30 +0100

    Fix super user

--- packages/citynexus/CityNexus/src/views/settings/_users.blade.php
+++ packages/citynexus/CityNexus/src/views/settings/_users.blade.php
@@ -121,7 +121,7 @@
             data: {
                 _token: '{{csrf_token()}}',
                 user:{
-                    admin: status
+                    super_admin: status
                 }
             }
         }).success(function(){

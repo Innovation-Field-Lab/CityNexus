10bcc8b941095aa488862d10e8e2df0c346bcbd3
Sean Alaback <sean.alaback@gmail.com>
2016-09-20 09:14:22 -0400

    Fix tag error

--- packages/citynexus/CityNexus/src/views/property/_tag.blade.php
+++ packages/citynexus/CityNexus/src/views/property/_tag.blade.php
@@ -1,6 +1,8 @@
 <div class="list-group-item" id="tag-{{$tag->id}}"
-     data-toggle="tooltip" data-placement="left" title="Tagged: {{date_format($tag->pivot->created_at,"m/d/Y")}} @if(null != $tag->pivot->created_by)
-        Tagged By: {{\App\User::find($tag->pivot->created_by)->fullname()}} @endif">
+    {{--TODO: Add following info--}}
+    {{--data-toggle="tooltip" data-placement="left" title="Tagged: {{date_format($tag->pivot->created_at,"m/d/Y")}} @if(null != $tag->pivot->created_by)--}}
+        {{--Tagged By: {{\App\User::find($tag->pivot->created_by)->fullname()}} @endif"--}}
+    >
     <i class="glyphicon glyphicon-tag"></i>
     {{$tag->tag}}
 <span class="pull-right">

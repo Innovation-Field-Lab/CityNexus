36ba52abf425ad8baa6c2d0a3ed39a70c45c5fc1
Sean Alaback <sean.alaback@gmail.com>
2016-10-29 18:18:02 -0400

    Fix properties edit/delete permissions

--- packages/citynexus/CityNexus/src/views/property/show.blade.php
+++ packages/citynexus/CityNexus/src/views/property/show.blade.php
@@ -56,9 +56,9 @@ $section = 'properties';
                     <ul class="dropdown-menu">
                         <li><a onclick="addFile()"> Add File</a></li>
                         <li><a onclick="addTask()">Add Task</a></li>
-                        @can('citynexus', ['property', 'edit'])<li><a onclick="editAddress()">Edit Address</a></li>@endcan
+                        @can('citynexus', ['properties', 'edit'])<li><a onclick="editAddress()">Edit Address</a></li>@endcan
                         <li><a href="{{action('\CityNexus\CityNexus\Http\TablerController@getMergeRecords')}}/{{$property->id}}">Merge Property</a></li>
-                        @can('citynexus', ['property', 'delete'])<li><a href="{{action('\CityNexus\CityNexus\Http\PropertyController@getDelete', ['id' => $property->id])}}/{{$property->id}}"><i class="fa fa-trash"></i> Delete Property</a></li>@endcan
+                        @can('citynexus', ['properties', 'delete'])<li><a href="{{action('\CityNexus\CityNexus\Http\PropertyController@getDelete', ['id' => $property->id])}}/{{$property->id}}"><i class="fa fa-trash"></i> Delete Property</a></li>@endcan
                     </ul>
                 </div>
                 @if($property->location_id != null && 'local' != env('APP_ENV'))

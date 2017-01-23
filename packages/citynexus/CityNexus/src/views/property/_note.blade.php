f2773b7834392cefb9dd2249517e1db473412277
Sean Alaback <sean.alaback@gmail.com>
2017-01-23 08:07:22 -0500

    Add roll over for real date posted.

--- packages/citynexus/CityNexus/src/views/property/_note.blade.php
+++ packages/citynexus/CityNexus/src/views/property/_note.blade.php
@@ -1,5 +1,6 @@
 <div class="list-group-item" id="note-{{$note->id}}">
     @if($note->creator->id == Auth::getUser()->id | Auth::getUser()->super_admin == true)<div style="color: red; cursor: pointer" class="glyphicon glyphicon-trash pull-right" onclick="deleteNote({{$note->id}})"></div>@endif
-    <small>{{$note->creator->fullname()}} - {{$note->created_at->diffForHumans()}}</small></P>
+    <small data-toggle="tooltip" data-placement="top" title="{{date_format($note->created_at,"m/d/Y")}}"
+    >{{$note->creator->fullname()}} - {{$note->created_at->diffForHumans()}}</small></P>
     <p>{{$note->note}}</p>
 </div>

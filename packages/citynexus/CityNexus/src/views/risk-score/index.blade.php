5d742216c55dfb1ea3be1abea9a87af603885567
Sean Alaback <sean.alaback@gmail.com>
2017-02-10 09:21:22 -0500

    Added pagination to score table

--- packages/citynexus/CityNexus/src/views/risk-score/index.blade.php
+++ packages/citynexus/CityNexus/src/views/risk-score/index.blade.php
@@ -18,7 +18,8 @@ $section = 'scores';
         {{--</div>--}}
 
         <div class="portlet-body">
-            <table class="table table-hover">
+            <div id="table-wrapper" class="hidden">
+                <table id="datatable" class="table table-striped table-bordered">
                     <thead>
                     <tr>
                         <th>Score Name</th>
@@ -28,46 +29,69 @@ $section = 'scores';
                     </thead>
                     <tbody>
                     @foreach($scores as $score)
-                            <tr>
-                                <th>{{$score->name}}</th>
-                                <td id="age-{{$score->id}}">{{$score->updated_at->diffForHumans()}}</td>
-                                <td>
-                                    @if($score->scope == 'custom_upload')
+                        <tr>
+                            <th>{{$score->name}}</th>
+                            <td id="age-{{$score->id}}">{{$score->updated_at->diffForHumans()}}</td>
+                            <td>
+                                @if($score->scope == 'custom_upload')
                                     <a class="btn btn-sm btn-primary" id="update-{{$score->id}}" href="{{action("\CityNexus\CityNexus\Http\RiskScoreController@getUpload")}}?score_id={{$score->id}}">Repload Score</a>
-                                    @else
+                                @else
                                     <span class="btn btn-sm btn-primary" onclick="updateScore({{$score->id}})" id="update-{{$score->id}}">Refresh Score</span>
-                                    @endif
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-default dropdown-toggle waves-effect" data-toggle="dropdown" aria-expanded="false"> Analysis <span class="caret"></span> </button>
-                                        <ul class="dropdown-menu">
-                                            <li><a href="{{action('\CityNexus\CityNexus\Http\ViewController@getDotMap')}}?is_score=true&score_id={{$score->id}}">Dot Map</a></li>
-                                            <li><a href="{{action('\CityNexus\CityNexus\Http\ViewController@getHeatMap')}}?table=citynexus_scores_{{$score->id}}&key=score">Heat Map</a></li>
-                                            <li><a href="{{action('\CityNexus\CityNexus\Http\RiskScoreController@getPinMap', ['score_id' => $score->id])}}">Pin Map</a></li>
-                                            <li><a href="{{action('\CityNexus\CityNexus\Http\ViewController@getDistribution', ['citynexus_scores_' . $score->id, 'score'])}}">Distribution Chart</a></li>
-                                            <li><a href="{{action('\CityNexus\CityNexus\Http\RiskScoreController@getRanking', ['score_id' => $score->id])}}">Rankings</a></li>
-                                        </ul>
-                                    </div>
+                                @endif
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default dropdown-toggle waves-effect" data-toggle="dropdown" aria-expanded="false"> Analysis <span class="caret"></span> </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="{{action('\CityNexus\CityNexus\Http\ViewController@getDotMap')}}?is_score=true&score_id={{$score->id}}">Dot Map</a></li>
+                                        <li><a href="{{action('\CityNexus\CityNexus\Http\ViewController@getHeatMap')}}?table=citynexus_scores_{{$score->id}}&key=score">Heat Map</a></li>
+                                        <li><a href="{{action('\CityNexus\CityNexus\Http\RiskScoreController@getPinMap', ['score_id' => $score->id])}}">Pin Map</a></li>
+                                        <li><a href="{{action('\CityNexus\CityNexus\Http\ViewController@getDistribution', ['citynexus_scores_' . $score->id, 'score'])}}">Distribution Chart</a></li>
+                                        <li><a href="{{action('\CityNexus\CityNexus\Http\RiskScoreController@getRanking', ['score_id' => $score->id])}}">Rankings</a></li>
+                                    </ul>
+                                </div>
 
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-default dropdown-toggle waves-effect" data-toggle="dropdown" aria-expanded="false"> Actions <span class="caret"></span> </button>
-                                        <ul class="dropdown-menu">
-                                            @unless($score->scope == 'custom_upload')
-                                                <li><a href="{{action('\CityNexus\CityNexus\Http\RiskScoreController@getEditScore', ['score_id' => $score->id])}}">Edit Score</a></li>
-                                                <li><a href="{{action('\CityNexus\CityNexus\Http\RiskScoreController@getDuplicateScore', ['score_id' => $score->id])}}">Duplicate Score</a></li>
-                                            @endunless
-                                            <li><a href="{{action('\CityNexus\CityNexus\Http\RiskScoreController@getRemoveScore', ['score_id' => $score->id])}}">Remove Score</a></li>
-                                        </ul>
-                                    </div>
-                                </td>
-                            </tr>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default dropdown-toggle waves-effect" data-toggle="dropdown" aria-expanded="false"> Actions <span class="caret"></span> </button>
+                                    <ul class="dropdown-menu">
+                                        @unless($score->scope == 'custom_upload')
+                                            <li><a href="{{action('\CityNexus\CityNexus\Http\RiskScoreController@getEditScore', ['score_id' => $score->id])}}">Edit Score</a></li>
+                                            <li><a href="{{action('\CityNexus\CityNexus\Http\RiskScoreController@getDuplicateScore', ['score_id' => $score->id])}}">Duplicate Score</a></li>
+                                        @endunless
+                                        <li><a href="{{action('\CityNexus\CityNexus\Http\RiskScoreController@getRemoveScore', ['score_id' => $score->id])}}">Remove Score</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
                     @endforeach
+                    </tbody>
                 </table>
             </div>
         </div>
+    </div>
 </div>
 @stop
 
+@push('style')
+<!-- DataTables -->
+<link href="/vendor/citynexus/plugins/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+@endpush
+
 @push('js_footer')
+<script src="/vendor/citynexus/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="/vendor/citynexus/plugins/datatables/dataTables.bootstrap.js"></script>
+
+<script src="/vendor/citynexus/pages/datatables.init.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#datatable').dataTable({
+            stateSave: true
+        });
+        $('#loading').addClass('hidden');
+        $('#table-wrapper').removeClass('hidden');
+    } );
+    TableManageButtons.init();
+
+</script>
 
 <script>
     function updateScore( id )
